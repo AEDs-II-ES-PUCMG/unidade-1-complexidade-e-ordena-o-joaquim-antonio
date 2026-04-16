8b2fcb488225d26d5bee22152099e3b8cf748ced
Joaquim Antonio <joaquim.antonio.camargos@gmail.com>
2026-04-15 21:06:13 -0300

    feat: adicionado busca binaria oficina de ordenacao

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -3,13 +3,15 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class SelectionSort<T extends Comparable<T>> implements IOrdenador<T>{
-    private long comparacoes;
+
+public class Bubblesort<T extends Comparable<T>> implements IOrdenador<T>{
+
+	private long comparacoes;
 	private long movimentacoes;
 	private LocalDateTime inicio;
 	private LocalDateTime termino;	
 	
-	public SelectionSort() {
+	public Bubblesort() {
 		comparacoes = 0;
 		movimentacoes = 0;
 	}
@@ -18,7 +20,7 @@ public class SelectionSort<T extends Comparable<T>> implements IOrdenador<T>{
 	public T[] ordenar(T[] dados) {
 		return ordenar(dados, T::compareTo);
 	}
-	
+
 	@Override
 	public T[] ordenar(T[] dados, Comparator<T> comparador) {
 		T[] dadosOrdenados = Arrays.copyOf(dados, dados.length);
@@ -26,21 +28,23 @@ public class SelectionSort<T extends Comparable<T>> implements IOrdenador<T>{
 		
 		inicio = LocalDateTime.now();
 		
-		for (int posReferencia = 0; posReferencia < tamanho ; posReferencia++) {
-            int posMenor = posReferencia;
-			for (int posicao = posReferencia+1; posicao < tamanho; posicao++) {
+		for (int posReferencia = tamanho - 1; posReferencia > 0; posReferencia--) {
+			int trocas = 0;
+			for (int posicao = 0; posicao < posReferencia; posicao++) {
 				comparacoes++;
-				if (comparador.compare(dadosOrdenados[posMenor],dadosOrdenados[posicao]) > 0){
-					posMenor = posicao;
+				if (comparador.compare(dadosOrdenados[posicao], dadosOrdenados[posicao+1]) > 0){
+					swap (posicao, posicao + 1, dadosOrdenados);
+					trocas++;
 				}
 			}
-			swap(posReferencia, posMenor, dadosOrdenados);
+			if(trocas == 0 )
+				posReferencia = 0;
 		}	
 		termino = LocalDateTime.now();
 
 		return dadosOrdenados;
 	}
-
+	
 	private void swap(int i, int j, T[] vet) {
 		movimentacoes++;
 		
